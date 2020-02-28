9d1ed9264802d8dc11e552cda21de3578b7ab62e
xg-a06 <xg_jser@163.com>
2020-02-28 15:45:02 +0800

    [mod] eslint启动细节

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import { hot } from 'react-hot-loader/root';
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 
 class App extends PureComponent {
   render () {
     return (
-      <Fragment>
+      <>
         app
-      </Fragment>
+      </>
     );
   }
 }
